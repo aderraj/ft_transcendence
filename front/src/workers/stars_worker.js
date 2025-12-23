5facfd4b2488a53335fb10c84fb340fa5bbd65b3
aderraj <>
2025-12-23 17:07:37 +0100

    Responsive background

--- front/src/workers/stars_worker.js
+++ front/src/workers/stars_worker.js
@@ -2,6 +2,26 @@ let     canvas;
 let     ctx;
 let     size = {w: 0, h: 0};
 const   stars = [];
+let     dpr = 1;
+
+function resizeCanvas(w, h) {
+    dpr = self.devicePixelRatio || 1;
+
+    canvas.width = Math.floor(w * dpr);
+    canvas.height = Math.floor(h * dpr);
+
+    canvas.styleWidth = w;
+    canvas.styleHeight = h;
+
+    ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
+
+    size.w = w;
+    size.h = h;
+
+    initStars(stars);
+}
+
+
 
 function initStars(stars) {
     stars.length = 0;
@@ -19,42 +39,39 @@ function initStars(stars) {
 }
 
 const render = () => {
-    ctx.fillStyle = "Black";
-    if (canvas.width != size.w)
-        canvas.width = size.w;
-    if (canvas.height != size.h)
-        canvas.height = size.h;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, size.w, size.h);
+
     ctx.fillStyle = "white";
-    for (let i = 0; i <= 500; i++) {
-        stars[i].x += stars[i].vx * stars[i].size;
-        stars[i].y += stars[i].vy * stars[i].size;
-        stars[i].x = stars[i].x < 0 ? canvas.width :
-                        stars[i].x > canvas.width ? 0 : stars[i].x;
-        stars[i].y = stars[i].y < 0 ? canvas.height :
-                        stars[i].y > canvas.height ? 0 : stars[i].y;
+    for (let i = 0; i < stars.length; i++) {
+        const s = stars[i];
+
+        s.x += s.vx * s.size;
+        s.y += s.vy * s.size;
+
+        if (s.x < 0) s.x = size.w;
+        if (s.x > size.w) s.x = 0;
+        if (s.y < 0) s.y = size.h;
+        if (s.y > size.h) s.y = 0;
+
         ctx.beginPath();
-        ctx.arc(stars[i].x,
-            stars[i].y,
-            stars[i].size,
-            0,
-            Math.PI * 2);
+        ctx.arc(s.x, s.y, s.size, 0, Math.PI * 2);
         ctx.fill();
     }
-    requestAnimationFrame(render);
-}
 
-self.onmessage = function(e) {
-    if (e.data.type  == 'init') {        
+    requestAnimationFrame(render);
+};
+
+self.onmessage = function (e) {
+    if (e.data.type === 'init') {
         canvas = e.data.canvas;
         ctx = canvas.getContext("2d");
-        size.w = canvas.width;
-        size.h = canvas.height;
-        initStars(stars);
+
+        resizeCanvas(canvas.width, canvas.height);
         render();
     }
-    else if (e.data.type == 'resize') {
-        size.w = e.data.w;
-        size.h = e.data.h;
+
+    if (e.data.type === 'resize') {
+        resizeCanvas(e.data.w, e.data.h);
     }
-}
+};
